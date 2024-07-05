added training options: rotation and flipping as augmentation, and using inpainting as pseudo ground truth. fixed testing script bug.

datasets/HK_dataset.py
@@ -9,9 +9,13 @@ from .mono_dataset import MonoDataset
 class HKInitDataset(MonoDataset):
     """Superclass for different types of KITTI dataset loaders
     """
-    def __init__(self, *args, distorted=False, **kwargs):
+    def __init__(self, *args, flipping = False, rotating = False, distorted=False, inpaint_pseudo_gt_dir = None, **kwargs):
         super(HKInitDataset, self).__init__(*args, **kwargs)
 
+        self.inpaint_pseudo_gt_dir = inpaint_pseudo_gt_dir
+        self.flipping = flipping
+        self.rotating = rotating
+        
         # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size.
         # To normalize you need to scale the first row by 1 / image_width and the second row
         # by 1 / image_height. Monodepth2 assumes a principal point to be exactly centered.
@@ -36,12 +40,14 @@ class HKInitDataset(MonoDataset):
         
         return False
     
-    def get_color(self, folder, frame_index, side, do_flip):
+    def get_color(self, folder, frame_index, side, do_flip, do_rot):
         color = self.loader(self.get_image_path(folder, frame_index, side))
 
-        # if do_flip:
-        #     color = color.transpose(pil.FLIP_LEFT_RIGHT)
-
+        if do_flip and self.flipping:
+            color = color.transpose(pil.FLIP_LEFT_RIGHT)
+        if do_rot and self.rotating:
+            angle = np.random.choice([pil.ROTATE_90, pil.ROTATE_180, pil.ROTATE_270])
+            color = color.transpose(angle)
         return color
     
 

datasets/mono_dataset.py
@@ -92,10 +92,10 @@ class MonoDataset(data.Dataset):
         """
         for k in list(inputs):
             frame = inputs[k]
-            if "color" in k:
+            if "color" in k or "inpaint_color" in k:
                 n, im, i = k
                 for i in range(self.num_scales):
-                    inputs[(n, im, i)] = self.resize[i](inputs[(n, im, i - 1)])
+                    inputs[(n, im, i)] = self.resize[i](inputs[(n, im, i - 1)])                
         
         if do_color_aug:
              fn_idx, brightness_factor, contrast_factor, saturation_factor, hue_factor = transforms.ColorJitter.get_params(
@@ -103,10 +103,10 @@ class MonoDataset(data.Dataset):
 
         for k in list(inputs):
             f = inputs[k]
-            if "color" in k:
+            if "color" in k or "inpaint_color" in k:
                 n, im, i = k
                 inputs[(n, im, i)] = self.to_tensor(f)
-                if do_color_aug:
+                if do_color_aug and "color" in k:
                     for fn_id in fn_idx:
                         if fn_id == 0 and brightness_factor is not None:
                             f = F.adjust_brightness(f, brightness_factor)
@@ -149,6 +149,7 @@ class MonoDataset(data.Dataset):
 
         do_color_aug = self.is_train and random.random() > 0.5
         do_flip = self.is_train and random.random() > 0.5
+        do_rot = self.is_train and random.random() > 0.5
 
         line = self.filenames[index].split()
         if len(line) == 1:
@@ -171,9 +172,15 @@ class MonoDataset(data.Dataset):
         for i in self.frame_idxs:
             if i == "s":
                 other_side = {"r": "l", "l": "r"}[side]
-                inputs[("color", i, -1)] = self.get_color(folder, frame_index, other_side, do_flip)
+                inputs[("color", i, -1)] = self.get_color(folder, frame_index, other_side, do_flip, do_rot)
+                if self.inpaint_pseudo_gt_dir is not None:
+                    inpainted_folder = folder.replace(self.data_path[0], self.inpaint_pseudo_gt_dir)
+                    inputs[("inpaint_color", i, -1)] = self.get_color(inpainted_folder, frame_index, other_side, do_flip, do_rot)
             else:
-                inputs[("color", i, -1)] = self.get_color(folder, frame_index + i, side, do_flip)
+                inputs[("color", i, -1)] = self.get_color(folder, frame_index + i, side, do_flip, do_rot)
+                if self.inpaint_pseudo_gt_dir is not None:
+                    inpainted_folder = folder.replace(self.data_path[0], self.inpaint_pseudo_gt_dir)
+                    inputs[("inpaint_color", i, -1)] = self.get_color(inpainted_folder, frame_index + i, side, do_flip, do_rot)
 
         # adjusting intrinsics to match each scale in the pyramid
         for scale in range(self.num_scales):
@@ -191,6 +198,7 @@ class MonoDataset(data.Dataset):
         for i in self.frame_idxs:
             del inputs[("color", i, -1)]
             del inputs[("color_aug", i, -1)]
+            del inputs[("inpaint_color", i, -1)]
 
         if self.load_depth:
             depth_gt = self.get_depth(folder, frame_index, side, do_flip)

test_simple.py
@@ -128,7 +128,7 @@ def parse_args():
                         help='use median scaling only on specular pixels')
     parser.add_argument("--notclipped", action='store_true',
                         help='skip clipping depth values')
-    parser.add_argument("--input_mask", type=str, default=None,)
+    parser.add_argument("--input_mask", type=str, default=None,) #this is only for visualization and not for masking image before putting in encoder
     parser.add_argument("--decompose", action='store_true',
                         help='use decompose model')
 
@@ -281,9 +281,10 @@ def test_simple(args, seq):
             pred_depth = (1/disp_resized).squeeze().cpu().numpy()
 
             if args.save_depth:
-                input_mask_np = input_mask[0, 0, :, :].numpy()
-                pred_depth[input_mask_np == 0] = 0
-                pred_depth[pred_depth > 0.3] = 0
+                if args.input_mask is not None:
+                    input_mask_np = input_mask[0, 0, :, :].numpy()
+                    pred_depth[input_mask_np == 0] = 0
+                    pred_depth[pred_depth > 0.3] = 0 #IMPORTANT:remove in some cases!
                 max_value = np.max(pred_depth)
                 trip_im = pil.fromarray(np.stack((pred_depth*255/max_value,)*3, axis=-1).astype(np.uint8))
                 # trip_im.save("outputimage.png")